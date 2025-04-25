956c79bd39769d428853ffb7d12fe6500eb2e838
Alireza200685 <150374827+Alireza200685@users.noreply.github.com>
2025-04-25 19:44:40 +0330

    app.py

--- app.py
+++ app.py
@@ -164,5 +164,5 @@ def find_route():
     return jsonify({"result": result})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
